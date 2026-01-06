2310e9e598e7034aa52dc5e8b108c7932c5bd42f
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-01-06 22:49:21 +0600

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
@@ -3,8 +3,12 @@ class Solution {
     bool checkKthBit(int n, int k) {
         //  code here
         
-        int ans = n >> k;
-        if((ans & 1) != 0) return true;
+        // int ans = n >> k;
+        // if((ans & 1) != 0) return true;
+        // else return false;
+        
+        int ans = 1 << k;
+        if((n & ans) != 0) return true;
         else return false;
         
         
